b0c4f4dd3079e64c60972c4d6a41096794386726
lpegram85 <32425123+lpegram85@users.noreply.github.com>
2018-06-28 11:22:17 -0700

    Add files via upload
    Visualization of Prospective Clients

---
+++ Prospective_Clients.py
@@ -0,0 +1,65 @@
+import plotly.plotly as py
+py.plotly.tools.set_credentials_file(username='lpegram', api_key='**********')
+
+
+import pandas as pd
+from IPython.display import display
+
+df = pd.read_csv(r'C:\Users\Lisa.Pegram\Downloads\ABA_Opp.csv')
+df['text']= df['Lat'].astype(str) + ', ' + df['Longitude'].astype(str) + ''
+
+# Success! Display the first 5 rows of the dataset
+display(df.head(n=5))
+display(df.info())
+
+# # # Store our latitude and longitude
+latitudes = df["Lat"]
+longitudes = df["Longitude"]
+#
+scl = [ [0,"rgb(5, 10, 172)"],[0.35,"rgb(40, 60, 190)"],[0.5,"rgb(70, 100, 245)"],\
+    [0.6,"rgb(90, 120, 245)"],[0.7,"rgb(106, 137, 247)"],[1,"rgb(220, 220, 220)"] ]
+
+data = [ dict(
+        type = 'scattergeo',
+        locationmode = 'USA-states',
+        lon = longitudes,
+        lat = latitudes,
+        text = df['text'],
+        mode = 'markers',
+        marker = dict(
+            size = 8,
+            opacity = 0.8,
+            reversescale = True,
+            autocolorscale = False,
+            symbol = 'circle',
+            line = dict(
+                width=1,
+                color='rgba(102, 102, 102)'
+            ),
+            colorscale = scl,
+            cmin = 0,
+            color = df['Count'],
+            cmax = df['Count'].max(),
+            colorbar=dict(
+                title="ABA Originating Prospects"
+            )
+        ))]
+
+layout = dict(
+        title = 'ABA Originating Prospects<br>(Hover for airport names)',
+        #colorbar = True,
+        colorbar=False,
+        geo = dict(
+            scope='usa',
+            projection=dict( type='albers usa' ),
+            showland = True,
+            landcolor = "rgb(250, 250, 250)",
+            subunitcolor = "rgb(217, 217, 217)",
+            countrycolor = "rgb(217, 217, 217)",
+            countrywidth = 0.5,
+            subunitwidth = 0.5
+        ),
+    )
+
+fig = dict( data=data, layout=layout )
+py.iplot( fig, validate=False, filename='ABA_geosegment' )
