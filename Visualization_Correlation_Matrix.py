b1da6970b139ce30c62fbc8023a6a61132c9fbde
lpegram85 <32425123+lpegram85@users.noreply.github.com>
2018-09-07 12:43:09 -0700

    Update Visualization_Correlation_Matrix.py

--- Visualization_Correlation_Matrix.py
+++ Visualization_Correlation_Matrix.py
@@ -9,6 +9,7 @@ matplotlib.style.use('ggplot')
 import pandas as pd
 from pandas import DataFrame
 from pandas.plotting import scatter_matrix
+import plotly.graph_objs as go
 
 df = pd.read_csv("C:\\Users\Lisa.Pegram\\Downloads\\MPI_national.csv",skiprows = 1, header=None)
 #url="https://archive.ics.uci.edu/ml/machine-learning-databases/undocumented/connectionist-bench/sonar/sonar.all-data"
@@ -42,3 +43,25 @@ print(df2.head(5))
 pd.plotting.scatter_matrix(df2, alpha=0.2, figsize=(12, 12),diagonal='kde')
 plt.show()
 
+print(df2.corr())
+corr=df2.corr()
+
+data = [
+    go.Surface(
+        z=corr.as_matrix()
+    )
+]
+layout = go.Layout(
+    title='3D-Visualization of the Correlation Matrix',
+    autosize=False,
+    width=500,
+    height=500,
+    margin=dict(
+        l=65,
+        r=50,
+        b=65,
+        t=90
+    )
+)
+fig = go.Figure(data=data, layout=layout)
+py.iplot(fig, filename='correlations-as-a-3D-surface')
